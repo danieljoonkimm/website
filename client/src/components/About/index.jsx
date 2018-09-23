7b335c5a64caacfd003c0b63335c44a27499291f
danieljoonkimm <danieljoonkimm@gmail.com>
2018-09-23 15:45:36 -0700

    [edit] editing aboutMe component

--- client/src/components/About/index.jsx
+++ client/src/components/About/index.jsx
@@ -19,10 +19,10 @@ class About extends Component {
                         I want to build a product that I'm passionate about - build a company around that product and grow
                         personally and professionally regardless of financial gains.
                         I've learned from past careers that money does not equal happiness.
-                        Some food for thought: passion/happiness is always more important than stability.
+                        Some food for thought: passion and happiness is always more important than stability.
                         Life has a great way of intervening in the way of well laid plans,
                         and the best plan is knowing that your plan could be broken; I
-                        took my chance when it was possible, and found what I truly wanted to do - I have found happiness</p>
+                        took my chance when it was possible, and found what I truly wanted to do - I have found my passion and happiness</p>
                 </div>
             </div>
         )
