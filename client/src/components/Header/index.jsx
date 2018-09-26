978048e848a5ca253413d4c55b2b75d42622d8c7
Daniel Kim <danieljoonkimm@gmail.com>
2018-09-25 17:45:09 -0700

    [add] adding slideshow background

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -24,6 +24,12 @@
 import React, { Component } from 'react';
 import Typed from 'react-typed';
 import './header.scss';
+import background1 from '../../../public/background1.jpg';
+import background2 from '../../../public/background2.jpg';
+import background3 from '../../../public/background3.jpg';
+import background4 from '../../../public/background4.jpg';
+import background5 from '../../../public/background5.jpg';
+
 
 class Header extends Component {
     constructor() {
@@ -35,15 +41,23 @@ class Header extends Component {
 
     render() {
         return(
-            <div>
-                <div className='wrapper'>
-                    <Typed className='wrapper-child'
-                        strings={['^1000 DANIEL KIM']}
-                        typeSpeed={80}
-                        showCursor={false}
-                    >
-                    </Typed>
-                </div>
+            <div className='slider'>
+                <figure>
+                    <img src={background1}/>
+                    <img src={background2}/>
+                    <img src={background3}/>
+                    <img src={background4}/>
+                    <img src={background5}/>
+                </figure>
+                    <div className='wrapper'>
+                        <Typed className='wrapper-child'
+                            strings={['^1000 DANIEL KIM']}
+                            typeSpeed={80}
+                            showCursor={false}
+                        >
+                        </Typed>
+                    </div>
+
 
                 
             </div>
