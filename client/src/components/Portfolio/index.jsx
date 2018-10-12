a5d62fba811b268bb58d3e9b887c10dea0bc2efd
Daniel Kim <danieljoonkimm@gmail.com>
2018-10-11 20:39:00 -0700

    [edit] fixing portfolio component

--- client/src/components/Portfolio/index.jsx
+++ client/src/components/Portfolio/index.jsx
@@ -17,14 +17,49 @@ class Portfolio extends Component {
 
     render () {
         return(
-            <div className="view view-tenth">
-                <img src={fakebook} />
-                <div className="mask">
-                    <h2>Fakebook</h2>
-                    <p>View Code</p>
-                    <a href="#" className="info">Read More</a>
+            <div className='portfolio'>
+                <div className="view view-tenth">
+                    <img src={fakebook} />
+                    <div className="mask">
+                        <h2>Fakebook</h2>
+                        <p><a href='https://github.com/Greenfield-DDCH'>View Code</a></p><br/>
+                        <p className="info">Read More</p>
+                    </div>
                 </div>
+
+                <div className='view view-tenth'>
+                    <img src={foodie} />
+                    <div className="mask">
+                        <h2>Foodie</h2>
+                        <p><a href='https://github.com/mdhiFoodie'>View Code</a></p><br/>
+                        <p className="info">Read More</p>
+                    </div>
+                </div>
+
+                <div className='view view-tenth'>
+                    <img src={slackclone} />
+                    <div className="mask">
+                        <h2>Slack Clone</h2>
+                        <p><a href='https://github.com/danieljoonkimm/Technical_Growth_Assessment'>View Code</a></p><br/>
+                        <p className="info">Read More</p>
+                    </div>
+                </div>
+
+                <div className='view view-tenth'>
+                    <img src={website} />
+                    <div className="mask">
+                        <h2>Website</h2>
+                        <p><a href='https://github.com/danieljoonkimm/website'>View Code</a></p><br/>
+                        <p className="info">Read More</p>
+                    </div>
+                </div>
+
+
+                
+
             </div>
+
+            
         )
     }
 }
