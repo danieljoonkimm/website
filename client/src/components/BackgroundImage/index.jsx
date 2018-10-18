5944c3028cf4191096e5697bf03fbe2b2695fa6e
Daniel Kim <danieljoonkimm@gmail.com>
2018-10-17 23:37:02 -0700

    [edit] fixing background for contact

--- client/src/components/BackgroundImage/index.jsx
+++ client/src/components/BackgroundImage/index.jsx
@@ -4,10 +4,12 @@ import './background.scss';
 
 class BackgroundImage extends Component {
     render () {
-        return <div className='background'>
-                    <div className='layer'>
+        return <div>
+                    <div className='background-headers'>
+                        <div className='contact-smaller-header'><h3>Feel free to email me!</h3></div>
+                        <div className='contact-header'><h1>GET IN CONTACT</h1></div>
                     </div>
-                </div>
+               </div>
     }
 }
 
