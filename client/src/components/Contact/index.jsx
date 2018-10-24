3a60feccac32c7704d586482d39114807c795670
Daniel Kim <danieljoonkimm@gmail.com>
2018-10-23 23:28:00 -0700

    [edit] fixing contacts component

--- client/src/components/Contact/index.jsx
+++ client/src/components/Contact/index.jsx
@@ -9,6 +9,12 @@ class BackgroundImage extends Component {
                         <div className='contact-smaller-header'><h3>Feel free to email me!</h3></div>
                         <div className='contact-header'><h1>GET IN CONTACT</h1></div>
                     </div>
+
+                    <div className='contact-info'>
+                        <div className='phoneNumber'>(323) 603-7431</div>
+                        <div className='email'>danieljoonkimm@gmail.com</div>
+                        <div className='from'>Los Angeles, CA</div>
+                    </div>
                </div>
     }
 }
