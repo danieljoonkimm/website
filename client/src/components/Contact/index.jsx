2c41e15d67dd806a07f2125741a9692e9d82a8cc
Daniel Kim <danieljoonkimm@gmail.com>
2018-10-30 00:34:51 -0700

    [add] adding modal to contact component

--- client/src/components/Contact/index.jsx
+++ client/src/components/Contact/index.jsx
@@ -83,8 +83,56 @@ class Contact extends Component {
           </GoogleMapReact>
         </div>
 
-        <div className='footerWrapper'>
-          <div className='footerContainer'>Daniel Kim © 2018</div>
+        <div className="footerWrapper">
+          <div className="footerContainer">Daniel Kim © 2018</div>
+        </div>
+
+        <div className="modal_container">
+          <button
+            type="button"
+            className="btn btn-info btn-lg"
+            data-toggle="modal"
+            data-target="#myModal"
+          >
+            Write Email
+          </button>
+
+          <div
+            className="modal fade"
+            id="myModal"
+            role="dialog"
+            backdrop="false"
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <button type="button" className="close" data-dismiss="modal">
+                    &times;
+                  </button>
+                  <h4 className="modal-title">Send Email to Daniel</h4>
+                </div>
+                <div className="modal-body">
+                  <textarea rows="4" cols="50" placeholder="write your message" />
+                </div>
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-default"
+                    data-dismiss="modal"
+                  >
+                    Send
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-default"
+                    data-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
