61f084b4f8002637f696e7461be0f034c675182e
Daniel Kim <danieljoonkimm@gmail.com>
2018-10-31 23:31:54 -0700

    [edit] fixing smooth scroll

--- client/src/components/Navigation/index.jsx
+++ client/src/components/Navigation/index.jsx
@@ -15,20 +15,20 @@ class Navigation extends Component {
   render() {
     return (
       <div className="container child topBotomBordersOut navbar-nav">
-        <ul>
-          <li className="nav-item active">
+        <ul className="smooth-scroll">
+          <li className="nav-item active" id="#">
             <a className="nav-link scroll" href="#">Home</a>
           </li>
-          <li className="nav-item">
+          <li className="nav-item" id="about">
             <a className="nav-link scroll" href="#about">About Me</a>
           </li>
-          <li className="nav-item">
+          <li className="nav-item" id="skills">
             <a className="nav-link scroll" href="#skills">Skills</a>
           </li>
-          <li className="nav-item">
+          <li className="nav-item" id="portfolio">
             <a className="nav-link scroll" href="#portfolio">Portfolio</a>
           </li>
-          <li className="nav-item">
+          <li className="nav-item" id="contact">
             <a className="nav-link scroll" href="#contact">Contact Me</a>
           </li>
         </ul>
